9a0bc65ad69ff793164e84e65446ed62ae35b896
Owen Chestnut <owenchestnut@Nutz-comp.local>
2023-10-29 18:23:06 -0400

    restructure edit-song

--- songModel.php
+++ songModel.php
@@ -10,10 +10,30 @@ class SongModel
 
   public function __addSong($username, $title, $artist, $rating)
   {
-    $add = "INSERT INTO ratings (title, artist) VALUES (?, ?)";
-    $add2 = $this->db->prepare($add);
-    $add2->execute([$username, $title, $artist, $rating]);
+    $insert_add = "INSERT INTO ratings (user, title, artist, rating) VALUES (?, ?, ?, ?)";
+    $prep_add = $this->db->prepare($insert_add);
+    $prep_add->execute([$username, $title, $artist, $rating]);
+
+    if ($prep_add->execute()) {
+      echo "Song updated successfully.";
+
+    } else {
+      echo "Error updating song: " . $prep_add->error;
+    }
   }
 
+  public function __editSong($id, $title, $artist, $rating)
+  {
+    $update_edit = "UPDATE ratings SET title = ?, artist = ?, rating = ? WHERE id = ?";
+    $prep_edit = $this->db->prepare($update_edit);
+    $prep_edit->execute("ssii", $title, $artist, $rating, $id);
+
+    if ($prep_edit->execute()) {
+      echo "Song updated successfully.";
+
+    } else {
+      echo "Error updating song: " . $prep_edit->error;
+    }
+  }
 }
 ?>
