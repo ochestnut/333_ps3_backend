8e036552c7876324c4926d12ec3aab81a578d076
Owen Chestnut <owenchestnut@Nutz-comp.local>
2023-10-29 18:26:56 -0400

    restructure delete-song

--- songModel.php
+++ songModel.php
@@ -12,7 +12,7 @@ class SongModel
   {
     $insert_add = "INSERT INTO ratings (user, title, artist, rating) VALUES (?, ?, ?, ?)";
     $prep_add = $this->db->prepare($insert_add);
-    $prep_add->execute([$username, $title, $artist, $rating]);
+    $prep_add->bind_param("sssi", $username, $title, $artist, $rating);
 
     if ($prep_add->execute()) {
       echo "Song updated successfully.";
@@ -26,13 +26,26 @@ class SongModel
   {
     $update_edit = "UPDATE ratings SET title = ?, artist = ?, rating = ? WHERE id = ?";
     $prep_edit = $this->db->prepare($update_edit);
-    $prep_edit->execute("ssii", $title, $artist, $rating, $id);
+    $prep_edit->bind_param("ssii", $title, $artist, $rating, $id);
 
     if ($prep_edit->execute()) {
       echo "Song updated successfully.";
 
     } else {
       echo "Error updating song: " . $prep_edit->error;
+    }
+  }
+
+  public function __deleteSong($id)
+  {
+    $delete = "DELETE FROM ratings WHERE id = ?";
+    $prep_delete = $this->db->prepare($delete);
+    $prep_delete->bind_param("i", $id);
+
+    if ($prep_delete->execute()) {
+      echo "Row deleted successfully.";
+    } else {
+      echo "Error deleting song: " . $prep_delete->error;
     }
   }
 }
