ec23fd3cac757c250d360372fb1dfc14993a5cf3
Owen Chestnut <owenchestnut@Nutz-comp.local>
2023-11-02 13:04:28 -0400

    reworking index cors

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 require __DIR__ . "/inc/bootstrap.php";
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: true');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  // Respond to preflight request
+  header('Access-Control-Allow-Origin:*');
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+  header('Access-Control-Allow-Headers: Content-Type, Authorization');
+  header('Access-Control-Allow-Credentials: true');
+  http_response_code(200);
+  exit;
+}
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
