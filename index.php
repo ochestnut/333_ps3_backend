85e04b46c7f11840f524df0d234a7aa49aba1dd2
Owen Chestnut <owenchestnut@Nutz-comp.local>
2023-11-01 20:06:28 -0400

    editting index for rerouting

--- index.php
+++ index.php
@@ -10,12 +10,22 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-if ((isset($uri[2]) && $uri[2] != 'song') || !isset($uri[3])) {
+if ((isset($uri[2]) && ($uri[2] != 'song' || $uri[2] != 'user')) || !isset($uri[3])) {
   header("HTTP/1.1 404 Not Found");
   exit();
 }
-require PROJECT_ROOT_PATH . "/Controller/Api/SongController.php";
-$objFeedController = new SongController();
-$strMethodName = $uri[3] . 'Action';
-$objFeedController->{$strMethodName}();
+if ($uri[2] == 'song') {
+  require PROJECT_ROOT_PATH . "/Controller/Api/SongController.php";
+  $objFeedController = new SongController();
+  $strMethodName = $uri[3] . 'Action';
+  $objFeedController->{$strMethodName}();
+} else if ($uri[2] == 'user') {
+  require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
+  $objFeedController = new UserController();
+  $strMethodName = $uri[3] . 'Action';
+  $objFeedController->{$strMethodName}();
+} else {
+  header("HTTP/1.1 404 Not Found");
+  exit();
+}
 ?>
